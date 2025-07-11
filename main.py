798250ce555127aa29e5e226f9e0d88a5402fa79
uran-content <uran.content@gmail.com>
2025-07-11 15:57:54 +0300

    quik fix for process_auto_payments

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ async def bot_lifecycle(bot: Bot, dp: Dispatcher):
 
     payScheduler.add_job(
         lambda: process_auto_payments(bot),
-        trigger=CronTrigger(hour=10, minute=0),
+        trigger=CronTrigger(hour=24, minute=0),
         id='auto_payments',
         replace_existing=True
     )
